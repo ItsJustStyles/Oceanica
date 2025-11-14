cb5fde6dec0f902e9aea6c3c54b030490314a2a9
ItsJustStyles <lacayopicado@gmail.com>
2025-11-13 22:25:46 -0600

    Estoy codificando xd

---
+++ src/main/java/com/mycompany/oceanica/EstoyCodificando.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.oceanica;
+
+/**
+ *
+ * @author lacay
+ */
+public class EstoyCodificando extends Ataque{
+
+    public EstoyCodificando(int dano, Tablero tablero) {
+        super(dano, tablero);
+    }
+
+    @Override
+    public void aplicarDano(Casilla celda) {
+        
+    }
+    
+    public void Microprocesador_x264(){
+        
+    }
+    
+    public void Slower(){
+        
+    }
+    
+    public void NoSePuedeMas(){
+        
+    }
+    
+}
