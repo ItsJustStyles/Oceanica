58ea820519c87aa4a3e1e6adc5d33c19abc82972
ItsJustStyles <lacayopicado@gmail.com>
2025-11-16 02:55:15 -0600

    Se crearon 2 nuevos ataques (Solo las clases, no tienen funcionalidad por el momento)

---
+++ src/main/java/com/mycompany/oceanica/HoraDeBalatrear.java
@@ -0,0 +1,18 @@
+package com.mycompany.oceanica;
+
+/**
+ *
+ * @author lacay
+ */
+public class HoraDeBalatrear extends Ataque{
+
+    public HoraDeBalatrear(int dano, Tablero tablero) {
+        super(dano, tablero);
+    }
+
+    @Override
+    public void aplicarDano(Casilla celda) {
+        
+    }
+    
+}
