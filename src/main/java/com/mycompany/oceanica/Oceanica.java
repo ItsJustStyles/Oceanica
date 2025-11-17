697df9c7343cc3b9ef2ce866cb04b9d1939686fd
ItsJustStyles <lacayopicado@gmail.com>
2025-11-17 14:52:13 -0600

    Mi bombo

--- src/main/java/com/mycompany/oceanica/Oceanica.java
+++ src/main/java/com/mycompany/oceanica/Oceanica.java
@@ -1,25 +1,9 @@
 package com.mycompany.oceanica;
 
-import org.glassfish.tyrus.server.Server;
+
 
 public class Oceanica {
     public static void main(String[] args) {
 
-        int port = Integer.parseInt(System.getenv("PORT"));
-
-        Server server = new Server("0.0.0.0", port, "/", null, GameWebSocket.class);
-
-        try {
-            server.start();
-            System.out.println("WebSocket abierto en: ws://0.0.0.0:" + port + "/game");
-
-            // Mantener vivo el servidor PARA SIEMPRE
-            Thread.currentThread().join();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            server.stop();
-        }
-    }
+}
 }
