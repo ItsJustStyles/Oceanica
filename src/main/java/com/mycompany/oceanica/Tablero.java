2e2841b952df8e42d4b7bd3b6da3d920a839213e
gabos <gabos@Gabriel>
2025-11-17 15:32:24 -0600

    Revert "no hacer dano si la celda esta muerta"
    This reverts commit 4a682011cf76aa00f3389e01b9e078f3617954bc.

--- src/main/java/com/mycompany/oceanica/Tablero.java
+++ src/main/java/com/mycompany/oceanica/Tablero.java
@@ -191,7 +191,7 @@ public class Tablero{
     public void recibirDanoLocacion(int fila, int columna, int dano){
         for(Casilla c : casillas){
             if(c.getX() == fila && c.getY() == columna){
-                if (c.esta_vivo())c.recibirAtaque(dano);
+                c.recibirAtaque(dano);
                 if(!c.esta_vivo()){
                     c.ponerX();
                 }
