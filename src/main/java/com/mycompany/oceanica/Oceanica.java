98e883fe6d607705d3189ceaa9812be07063d2e2
ItsJustStyles <lacayopicado@gmail.com>
2025-11-17 11:42:35 -0600

    Cambio xd

--- src/main/java/com/mycompany/oceanica/Oceanica.java
+++ src/main/java/com/mycompany/oceanica/Oceanica.java
@@ -2,28 +2,19 @@ package com.mycompany.oceanica;
 
 import org.glassfish.tyrus.server.Server;
 
-public class Oceanica {  
+public class Oceanica {
     public static void main(String[] args) {
 
-        int port = Integer.parseInt(System.getenv("PORT")); // Render asigna este puerto
+        int port = Integer.parseInt(System.getenv("PORT"));
 
         Server server = new Server("0.0.0.0", port, "/", null, GameWebSocket.class);
-        new WebPopupExternal("https://itsjuststyles.github.io/sans.github.io/");
 
         try {
             server.start();
-            System.out.println("Servidor WebSocket iniciado en puerto: " + port);
+            System.out.println("WebSocket abierto en: ws://0.0.0.0:" + port + "/game");
 
-            while (GameWebSocket.getLastResult().isEmpty()) {
-                try {
-                    Thread.sleep(100);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            System.out.println("Mensaje recibido: " + GameWebSocket.getLastResult());
-            System.out.println("Deteniendo servidor...");
+            // Mantener vivo el servidor PARA SIEMPRE
+            Thread.currentThread().join();
 
         } catch (Exception e) {
             e.printStackTrace();
