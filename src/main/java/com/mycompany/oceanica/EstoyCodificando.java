b61a0362776ef5ca3f9dca331c2a7bf9f465f16f
gabos <gabos@Gabriel>
2025-11-17 15:24:33 -0600

    ataques xocas falta probar

--- src/main/java/com/mycompany/oceanica/EstoyCodificando.java
+++ src/main/java/com/mycompany/oceanica/EstoyCodificando.java
@@ -23,16 +23,60 @@ public class EstoyCodificando extends Ataque{
     public void Microprocesador_x264(){
         ReproductorSonido sonido = new ReproductorSonido("/sonidos/Microprocesador.wav", refFrame);
         sonido.play();
+        final int MAX_FILAS = 20;
+        final int MAX_COLUMNAS = 30;
+
+        int centroX = 10;   
+        int centroY = 15;   
+        int maxR = 10;      
+
+        for (int x = 1; x <= MAX_FILAS; x++) {
+            for (int y = 1; y <= MAX_COLUMNAS; y++) {
+                int d = Math.abs(x - centroX) + Math.abs(y - centroY);         
+                if (d <= maxR && d % 2 == 0) {
+                    tablero.recibirDanoLocacion(x, y, 100);
+                }
+            }
+        }
     }
     
     public void Slower(){
         ReproductorSonido sonido = new ReproductorSonido("/sonidos/Slower.wav", refFrame);
         sonido.play();
+        final int MAX_FILAS = 20;
+        final int MAX_COLUMNAS = 30;
+
+        for (int x = 1; x <= MAX_FILAS; x++) {
+            for (int y = 1; y <= MAX_COLUMNAS; y++) {
+
+                boolean bordeArriba = (x == 1);
+                boolean bordeAbajo = (x == MAX_FILAS);
+                boolean bordeIzq = (y == 1);
+                boolean bordeDer = (y == MAX_COLUMNAS);
+
+                if (bordeArriba || bordeAbajo || bordeIzq || bordeDer) {
+                    tablero.recibirDanoLocacion(x, y, 100);
+                }
+            }
+        }
     }
     
     public void NoSePuedeMas(){
         ReproductorSonido sonido = new ReproductorSonido("/sonidos/NoSePuedeMas.wav", refFrame);
         sonido.play();
+        final int MAX_FILAS = 20;
+        final int MAX_COLUMNAS = 30;
+
+        int centroX = 10;
+        int centroY = 15;
+
+        for (int x = 1; x <= MAX_FILAS; x++) {
+            for (int y = 1; y <= MAX_COLUMNAS; y++) {
+                if (x == centroX || y == centroY) {
+                    tablero.recibirDanoLocacion(x, y, 100);
+                }
+            }
+        }
     }
     
     public void EstoNoEsUnJuego(){
